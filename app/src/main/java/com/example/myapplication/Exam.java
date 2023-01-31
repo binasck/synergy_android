2a8f2dfbb90af5de9d81b1c7010cb6c23e12bb66
binasbinu <binasarmc@gmail.com>
2023-01-31 17:57:01 +0530

    4

--- app/src/main/java/com/example/myapplication/Exam.java
+++ app/src/main/java/com/example/myapplication/Exam.java
@@ -2,12 +2,27 @@ package com.example.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class Exam extends AppCompatActivity {
+    TextView questions;
+    Button next;
+    RadioButton option1,option2,option3,option4,correct_answer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exam);
+        questions=(TextView) findViewById(R.id.textView38);
+        next =(Button) findViewById(R.id.button3);
+        option1=(RadioButton) findViewById(R.id.radioButton);
+        option2=(RadioButton) findViewById(R.id.radioButton2);
+        option3=(RadioButton) findViewById(R.id.radioButton3);
+        option4=(RadioButton) findViewById(R.id.radioButton4);
+        correct_answer=(RadioButton) findViewById(R.id.radioButton5);
     }
 }
